e2dad08f48e6c15fcaaf7785f08184a45d8a464e
YANM <yamid.nino02@uptc.edu.co>
2023-09-17 00:05:41 -0500

    Creación de la vista de login para el administrador, aún no se crea configura el footer y falta mejorar los estilos.

---
+++ src/app/material.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from "@angular/core";
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatListModule } from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+
+const cateModules = [
+  MatToolbarModule,
+  MatSidenavModule,
+  MatButtonModule,
+  MatMenuModule,
+  MatListModule,
+  MatInputModule,
+  MatCardModule,
+];
+
+@NgModule({
+  imports: [...cateModules],
+  exports: [...cateModules],
+})
+
+export class MaterialModule { }
