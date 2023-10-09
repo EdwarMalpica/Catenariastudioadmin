d2f281c623145e36bc04cef9bfe96c1987eb3f71
EstebanNovoa <esteban.novoa@uptc.edu.co>
2023-10-08 21:52:37 -0500

    Update status appoitment completed and visual upgrades

--- src/app/data_management/data_manager.ts
+++ src/app/data_management/data_manager.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
-import {HttpClient, HttpParams} from "@angular/common/http";
+import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
 import { Observable } from "rxjs";
-import { AppointmentList } from "./appointment_model";
+import { Appointment, AppointmentList } from "./appointment_model";
 
 
 const apiUrl = "http://localhost:8000/api/"
@@ -14,9 +14,16 @@ export class DataManagerService {
   constructor(private http: HttpClient) {}
 
   public getAppontensBy(search_ep:string, params: HttpParams = new HttpParams()): Observable<AppointmentList> {
-    console.log("RUTAA : " ,  apiUrl+search_ep)
     return this.http.get<AppointmentList>(apiUrl+search_ep, {params});
   }
+
+  public editAppointment(search_ep: string, body: any, params: HttpParams = new HttpParams()): Observable<Appointment> {
+    const headers = new HttpHeaders().set('Content-Type', 'application/json');
+    const options = { params, headers };
+    return this.http.post<Appointment>(apiUrl + search_ep, body, options);
+  }
+
+
 
 
 
